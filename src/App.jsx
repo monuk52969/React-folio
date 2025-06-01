5bfe6a3118d48474f3624c908de64cc02b867b5d
Monu Kumar <monuk52069@gmail.com>
2025-06-01 14:51:25 +0530

    cursor close

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <Navbar />
         <Hero />
-        <SplashCursor />
+        {/* <SplashCursor /> */}
         <Technologies />
         <Projects />
         <Experience />
