11a8dc8cc789f2a7b40618233ac6b4abec692964
Monu Kumar <monuk52069@gmail.com>
2025-06-01 15:33:05 +0530

    remove lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useEffect } from 'react'
-import Lenis from '@studio-freight/lenis'
-
 import Navbar from './constants/components/Navbar'
 import Hero from './constants/components/Hero'
-import SplashCursor from "./customComponents/SplashCursor/SplashCursor"
 import Technologies from './constants/components/Technologies'
 import Projects from './constants/components/Projects'
 import Experience from './constants/components/Experience'
 import Contact from './constants/components/Contact'
 
 const App = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-  duration: 1.2,
-  easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  smooth: true,
-  smoothTouch: true, // ✅ This line fixes mobile scroll
-  gestureOrientation: 'vertical',
-})
-
-    const raf = (time) => {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-
-    return () => {
-      lenis.destroy()
-    }
-  }, [])
-
   return (
     <div className="min-h-screen overflow-x-hidden text-stone-300 antialiased">
       {/* 🌌 Background for whole site */}
